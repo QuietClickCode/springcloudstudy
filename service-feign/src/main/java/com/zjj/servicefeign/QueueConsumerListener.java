234afe22185bef42cfef50b8228641e2fcf548a4
QuietClickCode <2572417548@qq.com>
2020-02-05 23:00:51 +0800

    结合activemq成功

--- service-feign/src/main/java/com/zjj/servicefeign/QueueConsumerListener.java
+++ service-feign/src/main/java/com/zjj/servicefeign/QueueConsumerListener.java
@@ -3,8 +3,7 @@ package com.zjj.servicefeign;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
 @Component
-public class QueueConsumerListener
-{
+public class QueueConsumerListener {
     //queue模式的消费者
     @JmsListener(destination="${spring.activemq.queue-name}", containerFactory="queueListener")
     public void readActiveQueue(String message) {
